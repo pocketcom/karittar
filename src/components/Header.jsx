2b4c61c462e408e7f728a8a8059662459e19ddb4
Apple <apple@appurunoMacBook-puro.local>
2020-05-18 08:01:30 +0900

    icon change

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,6 @@ import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import AtmIcon from '@material-ui/icons/Atm';
 
 
 const useStyles = makeStyles((theme) => ({
@@ -22,7 +21,6 @@ export default function Header() {
     <div className={classes.root}>
       <AppBar position="static">
         <Toolbar variant="dense">
-            <AtmIcon fontSize="large"/>
           <Typography className="headerName" variant="h6" color="inherit">
                karitter
           </Typography>
